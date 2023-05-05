47ef9fbc7ec84fec339f08bd88194ffe47618e19
norberto5 <admin@norberto5.pl>
2023-05-05 13:18:24 +0200

    Added links to copy name and pwsh cmdlet to check user accounts in AAD

--- README.md
+++ README.md
@@ -1,8 +1,52 @@
 # Skrypty pomocnicze do Jiry ZHP
 Skrypty pomocnicze JavaScript do wtyczki Violentmonkey dla Jiry ZHP.
 
+## Obecne funkcjonalności
+- automatyczne dodawanie do zgłoszeń linków do:
+    - sprawdzenia profilu Tipi (zgłoszenia z numerem ewidencyjnym)
+    - skopiowania imienia i nazwiska zgłaszającego
+    - skopiowania cmdleta PowerShell do sprawdzenia istniejących kont w AAD
+
 # Instalacja
 1. Zainstaluj wtyczkę Violentmonkey (https://violentmonkey.github.io/)
 2. Dodaj we wtyczce skrypt z URL:
-- https://raw.githubusercontent.com/norberto5/zhp_jira_helpers/main/tipi_profile_url.js
-3. Gotowe, wejdź w jakieś zgłoszenie dotyczące utworzenia konta/resetu hasła/resetu uwierzytelniania dwuskładnikowego, w którym podany jest numer ewidencyjny, aby automatycznie dodał się parametr z linkiem do profilu tej osoby w Tipi.
+```
+https://raw.githubusercontent.com/norberto5/zhp_jira_helpers/main/tipi_profile_url.js
+```
+3. Gotowe, wejdź w jakieś zgłoszenie dotyczące utworzenia konta/resetu hasła/resetu uwierzytelniania dwuskładnikowego, w którym podany jest numer ewidencyjny, aby automatycznie dodał się parametr z linkiem do profilu tej osoby w Tipi. Ponadto dodaje się link do skopiowania imienia i nazwiska, a także link do skopiowania cmdleta do wyszukania kont w AAD w PowerShellu.
+
+# Sprawdzanie w PowerShellu
+
+Można sprawdzić czy konto użytkownika już istnienie w naszym AAD. Aby to zrobić należy uruchomić lokalnie terminal PowerShell.
+
+## Pierwsza instalacja modułu
+```
+Install-Module ExchangeOnlineManagement
+```
+
+## Importowanie modułu i połączenie się z Exchangem
+```
+Import-Module ExchangeOnlineManagement
+Connect-ExchangeOnline
+```
+
+## Używanie
+Jak już zaimportujemy moduł i połączymy się z Exchangem to wystarczy kliknąć w zgłoszeniu "Skopiuj sprawdzenie w Powershell" i wkleić kod (Ctrl + V) do terminala, a następnie go uruchomić (Enter).
+
+Kod generowany do sprawdzenia:
+
+``Get-User -Filter {DisplayName -like "Imię Nazwisko"}| Select DisplayName,UserPrincipalName,Title,Office,Department | Format-Table``
+
+Rezultat sprawdzenia wygląda mniej więcej tak:
+```
+DisplayName         UserPrincipalName               Title               Office                      Department
+-----------         -----------------               -----               ------                      ----------
+Krzysztof XYZ       k.xyz@zhp.net.pl                                    BABIMOJSKO-SULECHOWSKI      1080080053000
+Krzysztof XYZ       krzysztof.xyz2@zhp.net.pl                           Chorągiew Zachodniopomorska Hufiec Szczecin
+Krzysztof XYZ       krzysztof.xyz4@zhp.net.pl       AA000000000         WARSZAWA-PRAGA-POLUDNIE     1340240000000
+Krzysztof XYZ       krzysztof.xyz5@zhp.net.pl                           MORAG                       1280130000000
+Krzysztof XYZ       krzysztof.xyz@zhp.net.pl        AC000000000         JAWOR                       1020080000000
+Krzysztof XYZ       krzysztof.xyz6@zhp.net.pl                           Hufiec Kamienna Góra
+```
+
+Jeżeli cmdlet nic nie zwróci to znaczy, że nie mamy w AAD konta z takim imieniem i nazwiskiem. Oczywiście istnieje ryzyko, że istnieje jakieś konto, w którym jest literówka i wówczas go nie odnajdziemy.
--- tipi_profile_url.js
+++ tipi_profile_url.js
@@ -1,72 +1,150 @@
 // ==UserScript==
-// @name        Add link on JIRA issues to Tipi user profile
+// @name        Add links on JIRA issues to Tipi profile and to copy cmdlet to check existing AAD accounts
 // @namespace   Violentmonkey Scripts
-// @match       https://jira.zhp.pl/*
-// @grant       none
-// @version     1.4
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.6.0/jquery.min.js
+// @include     https://jira.zhp.pl/*
+// @version     2.0
 // @author      Norbert Piątkowski
 // @homepageURL https://github.com/norberto5/zhp_jira_helpers
 // @supportURL  https://github.com/norberto5/zhp_jira_helpers/issues
 // @downloadURL https://raw.githubusercontent.com/norberto5/zhp_jira_helpers/main/tipi_profile_url.js
-// @description Last update: 11.10.2021 - added support for Tipi HD tickets
+// @description Last update: 05.05.2023 - added user name copying link and Powershell cmdlet search copying link
 // ==/UserScript==
 
+JIRA.bind(JIRA.Events.ISSUE_REFRESHED, addLinks);
+JIRA.bind(JIRA.Events.REFRESH_ISSUE_PAGE, addLinks);
+addLinks();
 
-JIRA.bind(JIRA.Events.ISSUE_REFRESHED, addLinkToUserProfile);
-JIRA.bind(JIRA.Events.REFRESH_ISSUE_PAGE, addLinkToUserProfile);
-addLinkToUserProfile();
-
-function addLinkToUserProfile()
+function addLinks()
 {
   var propertyList = document.getElementsByClassName("property-list")[0];
 
-  if(propertyList != null)
+  if(propertyList == null)
   {
-    let userId = getUserId();
-    
-    var profileUrl = "";
-    if(isNaN(userId))
-    {
-      profileUrl = "<b>BŁĄD</b>";
-    }
-    else if(!userId)
-    {
-      return;
-    }
-    else
-    {
-      var userIdString = userId.toString();
-      profileUrl = `<a href="https://tipi.zhp.pl/persons/${userIdString}/detail#data" target="_blank" title="Śledź łącze">https://tipi.zhp.pl/persons/${userIdString}/detail#data</a>`;
-    }
-    
-    var newElement = document.createElement("li");
-    newElement.innerHTML += `<div class="wrap"><strong title="URL" class="name">URL:</strong><div class="value">${profileUrl}</div></div>`;
-    newElement.classList.add("item");
-    
-    propertyList.appendChild(newElement);
+    return;
   }
+
+  var linkElement = createLinkToUserProfile();
+  if(linkElement)
+  {
+    propertyList.appendChild(linkElement);
+  }
+
+  var copyElement = createCopyUserName();
+  if(copyElement)
+  {
+    propertyList.appendChild(copyElement);
+  }
+
+  var pwshElement = createCopyPowershellCheck();
+  if(pwshElement)
+  {
+    propertyList.appendChild(pwshElement);
+  }
+}
+
+function createLinkToUserProfile()
+{
+  let userId = getUserId();
+
+  var profileUrl = "";
+  if(isNaN(userId))
+  {
+    profileUrl = "<b>BŁĄD</b>";
+  }
+  else if(!userId)
+  {
+    return;
+  }
+  else
+  {
+    var userIdString = userId.toString();
+    profileUrl = `<a href="https://tipi.zhp.pl/persons/${userIdString}/detail#data" target="_blank" title="Śledź łącze">Profil w Tipi</a>`;
+  }
+
+  var newElement = document.createElement("li");
+  newElement.innerHTML += `<div class="wrap"><strong title="URL" class="name">Tipi:</strong><div class="value">${profileUrl}</div></div>`;
+  newElement.classList.add("item");
+  return newElement;
+}
+
+function createCopyUserName()
+{
+  var userName = getUserName();
+
+  if(!userName || userName == " ")
+  {
+    return;
+  }
+
+  var newElement = document.createElement("li");
+  var searchUrl = `<a onclick="navigator.clipboard.writeText('${userName}')">Skopiuj ''${userName}''</a>`;
+  newElement.innerHTML += `<div class="wrap"><strong title="URL" class="name">Kopiuj do schowka:</strong><div class="value">${searchUrl}</div></div>`;
+  newElement.classList.add("item");
+  return newElement;
+}
+
+function createCopyPowershellCheck()
+{
+  var userName = getUserName();
+
+  if(!userName || userName == " ")
+  {
+    return;
+  }
+
+  var newElement = document.createElement("li");
+
+  var pwsh = "";
+  pwsh = "Get-User -Filter {DisplayName -like &quot;" + userName + "&quot;}| Select DisplayName,UserPrincipalName,Title,Office,Department | Format-Table";
+
+  var searchUrl = `<a onclick="navigator.clipboard.writeText('${pwsh}')">Skopiuj sprawdzenie w Powershell</a>`;
+  newElement.innerHTML += `<div class="wrap"><strong title="URL" class="name">Powershell:</strong><div class="value">${searchUrl}</div></div>`;
+  newElement.classList.add("item");
+  return newElement;
 }
 
 function getUserId()
 {
   var propertyField = document.getElementById("customfield_10611-val");
-  
+
   if(!propertyField)
   {
     propertyField = document.getElementById("customfield_10615-val");
-    
+
     if(!propertyField)
     {
       return "";
     }
   }
-  
+
   var zhpId = propertyField.innerText;
-  
+
   if(!zhpId)
   {
-    return "";  
+    return "";
   }
-  
+
+  if(zhpId.length != 11)
+  {
+    return NaN;
+  }
+
   return (zhpId.substr(5) * 1);
+}
+
+function getUserName()
+{
+  var firstNameField = document.getElementById("customfield_10702-val");
+  var lastNameField = document.getElementById("customfield_10713-val");
+
+  if(!firstNameField || !lastNameField)
+  {
+    return "";
+  }
+
+  var firstName = firstNameField.innerText;
+  var lastName = lastNameField.innerText;
+
+  return firstName + " " + lastName;
 }
